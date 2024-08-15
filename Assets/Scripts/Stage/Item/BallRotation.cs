9aa8cdd11f58e24d637ea6a2667357dbafc80030
tibigamePoppo <ryosukehashimoto1@gmail.com>
2024-08-15 12:35:08 +0900

    一部のシェーダーをスクリプトに変更

---
+++ Assets/Scripts/Stage/Item/BallRotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BallRotation : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody _rigidBody;
+    [SerializeField]
+    private float _rotateSpeed = 5;
+    private void FixedUpdate()
+    {
+        var velocity = _rigidBody.velocity;
+        var spinAxis = Vector3.Cross(velocity, -Vector3.up).normalized;
+        transform.Rotate(spinAxis * _rotateSpeed);
+
+    }
+}
